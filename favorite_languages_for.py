2f69b4f25ad1e8471a602b87f94502d911a9ec99
Pylearn2017 <ylearn2017@users.noreply.github.com>
2019-10-03 20:29:36 +0300

    sdgsdgsdf
    gsdfgsdfg

---
+++ favorite_languages_for.py
@@ -0,0 +1,15 @@
+favorite_languages = {
+	'Alex':'python',
+	'Ivan':'c',
+	'Max':'ruby',
+	'Olya':'python'
+}
+
+for key, value in favorite_languages.items():
+	print(key +' - ' + value)
+
+for name in favorite_languages.keys():
+	print(name)
+
+for lag in favorite_languages.values():
+	print(lag)	
---
+++ simple_dictionary.py
@@ -0,0 +1,12 @@
+simple_dictionary = {}
+alien_0 = {
+	'color' : 'green',
+	'points' : 5
+}
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+alien_0['color'] = 'yellow'
+alien_0['speed'] = 'slow'
+print(alien_0)
+del alien_0['speed']
+print(alien_0)
